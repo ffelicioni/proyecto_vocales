4c1720cefdf91bcad0f14847437d2754820232b3
ffelicioni <ffelicioni@fi.uba.edu.ar>
2021-12-29 17:00:58 -0300

    prueba

--- src/funciones/fwav2df.py
+++ src/funciones/fwav2df.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-def extract_formants(file_path):  
+
+def extract_formants(file_path):
     import librosa
     from librosa import lpc
     from scipy import signal
@@ -18,35 +19,31 @@ def extract_formants(file_path):
     A = lpc(x, P)
     B=[1]
     B=np.append(B,np.zeros(P)) #[1 y p ceros]
-    
     s1 = signal.TransferFunction(B, A,dt=1/Fs)
     w,mag, phase = signal.dbode(s1,n=2000)
-    
     peaks, nn = find_peaks(mag)
     fmaximos=w[peaks]/(2*np.pi)
     if (fmaximos[0]>200):
         formant_freq=np.array([fmaximos[0], fmaximos[1]])
     else:
         formant_freq=np.array([fmaximos[1], fmaximos[2]])
-    
     return pd.DataFrame(formant_freq).transpose()
 
-
-def prepare_dataframe(folder_contents):
-    import numpy as np
-    import pandas as pd
-    from pathlib import Path
-    df=pd.DataFrame()
-    for file_path in folder_contents:
-        filename = Path(file_path).stem
-        filename_parts = filename.split('_')
-        metadata = {'file_path': file_path,
-                  'vocal': filename_parts[0],
-                  'id': filename_parts[1]}
-        df1=extract_formants(file_path)
-        df1['target']=filename_parts[0]
-        df=pd.concat([df, df1], axis=0).reset_index(drop=True)
-
-    df['target']=df['target'].str.upper()
-    df.sort_values(by=['target'], ascending=True,inplace=True)
-    return df
+#def prepare_dataframe(folder_contents):
+#    import numpy as np
+#    import pandas as pd
+#    from pathlib import Path
+#    df=pd.DataFrame()
+#    for file_path in folder_contents:
+#        filename = Path(file_path).stem
+#        filename_parts = filename.split('_')
+#        metadata = {'file_path': file_path,
+#                  'vocal': filename_parts[0],
+#                  'id': filename_parts[1]}
+#        df1=extract_formants(file_path)
+#        df1['target']=filename_parts[0]
+#v       df=pd.concat([df, df1], axis=0).reset_index(drop=True)
+#
+#    df['target']=df['target'].str.upper()
+#   df.sort_values(by=['target'], ascending=True,inplace=True)
+#    return df
