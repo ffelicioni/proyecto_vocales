28dba26fb47f0ed9355fec788d9acb4a224706c0
ffelicioni <ffelicioni@fi.uba.edu.ar>
2021-12-29 18:25:01 -0300

    read audios procesados

--- src/fwav2df.py
+++ src/fwav2df.py
@@ -62,3 +62,4 @@ def read_audios_procesados(url):
         df1=pd.read_csv(url+vocal+'.csv',header=None)
         df1['target']=vocal
         df=pd.concat([df, df1], axis=0).reset_index(drop=True)
+    return df
